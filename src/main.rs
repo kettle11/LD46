9690f397255c04c6e11e6ab6b8f170c6f5495948
Ian Kettlewell <ian.kettlewell@gmail.com>
2020-04-18 16:12:19 -0700

    Added way to record mouse inputs and play them back

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ fn main() {
     event_loop.run_async(app, run);
 }
 
+#[derive(Debug, Clone, Copy)]
 struct Color {
     r: f32,
     g: f32,
@@ -43,7 +44,14 @@ struct Ball {
     velocity: Vector3,
     radius: f32,
     color: Color,
-    intersection_position: Vector3,
+}
+
+#[derive(Debug)]
+struct Collectible {
+    position: Vector3,
+    radius: f32,
+    color: Color,
+    collected: bool,
 }
 
 struct Camera {
@@ -51,6 +59,19 @@ struct Camera {
     view: Matrix4x4,
     inverse_projection: Matrix4x4,
     inverse_view: Matrix4x4,
+}
+
+struct Level {
+    line_color: Color,
+    collectibles: Vec<Collectible>,
+}
+
+impl Level {
+    pub fn reset(&mut self) {
+        for collectible in &mut self.collectibles {
+            collectible.collected = false;
+        }
+    }
 }
 
 impl Camera {
@@ -89,11 +110,25 @@ impl Ball {
             {
                 let normal_of_collision = (self.position - p).normal();
                 let velocity_along_collision = Vector3::dot(normal_of_collision, self.velocity);
-                self.velocity -= normal_of_collision * velocity_along_collision;
+                if velocity_along_collision < 0.0 {
+                    self.velocity -= normal_of_collision * velocity_along_collision * 1.4;
+                }
                 self.position += normal_of_collision * 0.0001;
             }
         }
         self.position += self.velocity;
+    }
+
+    fn check_for_collectibles(&mut self, collectibles: &mut [Collectible]) {
+        for collectible in collectibles {
+            if !collectible.collected
+                && (self.position - collectible.position).length()
+                    < self.radius + collectible.radius
+            {
+                log!("COLLECT!");
+                collectible.collected = true;
+            }
+        }
     }
 }
 
@@ -108,6 +143,133 @@ fn point_with_line_segment(p: Vector3, a: Vector3, b: Vector3) -> (f32, Vector3)
     ((p - position).length(), position)
 }
 
+struct MouseState {
+    position: Vector2,
+    frame: u32,
+    mouse_up: bool,
+}
+
+struct MousePlayback {
+    state: Vec<MouseState>,
+    current_state: usize,
+    current_frame_recording: u32,
+    playing: bool,
+}
+
+impl MousePlayback {
+    pub fn new() -> Self {
+        Self {
+            state: Vec::new(),
+            current_state: 0,
+            current_frame_recording: 0,
+            playing: false,
+        }
+    }
+
+    pub fn increment_frame(&mut self) {
+        self.current_frame_recording += 1;
+    }
+
+    pub fn record_mouse(&mut self, position: Vector2) {
+        self.state.push(MouseState {
+            position,
+            frame: self.current_frame_recording,
+            mouse_up: false,
+        });
+    }
+
+    pub fn record_mouse_up(&mut self) {
+        self.state.push(MouseState {
+            position: Vector2::new(0., 0.),
+            frame: self.current_frame_recording,
+            mouse_up: true,
+        });
+    }
+
+    pub fn reset_playback(&mut self) {
+        self.current_state = 0;
+    }
+
+    pub fn playback(&mut self, frames: u32, lines: &mut Lines) {
+        // This isn't quite right.
+        if self.current_state < self.state.len() {
+            let target_frame = self.state[self.current_state].frame + frames;
+
+            while self.current_state < self.state.len()
+                && self.state[self.current_state].frame < target_frame
+            {
+                let current_state = &self.state[self.current_state];
+                // Play next action
+                let position = current_state.position;
+                if current_state.mouse_up {
+                    lines.end_segment();
+                } else {
+                    lines.add_segment(Vector3::new(position.x, position.y, 0.0));
+                }
+                self.current_state += 1;
+            }
+        }
+    }
+}
+
+struct Lines {
+    last_position: Option<Vector3>,
+    line_points: Vec<Vector3>,
+    needs_update: bool,
+    mesh: Mesh,
+}
+
+impl Lines {
+    pub fn new(gl: &GL) -> Self {
+        Self {
+            needs_update: false,
+            last_position: None,
+            line_points: Vec::new(),
+            mesh: Mesh::new(&gl),
+        }
+    }
+    pub fn end_segment(&mut self) {
+        self.last_position = None;
+    }
+
+    pub fn add_segment(&mut self, position: Vector3) {
+        self.needs_update = true;
+        if let Some(last_position_inner) = self.last_position {
+            if (last_position_inner - position).length() > 0.01 {
+                self.line_points.push(Vector3::new(
+                    last_position_inner.x,
+                    last_position_inner.y,
+                    0.0,
+                ));
+                self.line_points
+                    .push(Vector3::new(position.x, position.y, 0.0));
+
+                self.last_position = Some(position);
+            }
+        } else {
+            // Add a point
+            self.last_position = Some(position);
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.needs_update = true;
+        self.line_points.clear();
+    }
+
+    pub fn update_mesh(&mut self, gl: &GL) {
+        if self.needs_update {
+            self.needs_update = false;
+            lines::update_mesh_with_line(
+                &gl,
+                &mut self.mesh,
+                &self.line_points,
+                LINE_RADIUS,
+                Vector3::FORWARD,
+            );
+        }
+    }
+}
 async fn run(app: Application, mut events: Events) {
     let window = app.new_window().build().unwrap();
     let (mut gl_context, gl) = gl::setup(&window);
@@ -117,35 +279,11 @@ async fn run(app: Application, mut events: Events) {
     let frag = include_str!("shaders/frag.fs");
 
     let shader_program = ShaderProgram::new(&gl, vert, frag);
-    let mut quad = Mesh::new(&gl);
-    quad.update(
-        &gl,
-        &[
-            Vector3::new(0., 0., 0.),
-            Vector3::new(1., 0., 0.),
-            Vector3::new(1., 1., 0.),
-            Vector3::new(0., 1., 0.),
-        ],
-        &[[0, 1, 2], [0, 2, 3]],
-    );
-    let mut line_mesh = Mesh::new(&gl);
 
-    let mut line_points = Vec::new();
-
-    // line_points.push(Vector3::new(0.0, -0.3, 0.0));
-    // line_points.push(Vector3::new(0.5, -0.3, 0.0));
-
-    lines::update_mesh_with_line(
-        &gl,
-        &mut line_mesh,
-        &line_points,
-        LINE_RADIUS,
-        Vector3::FORWARD,
-    );
     let mut screen_width = 0;
     let mut screen_height = 0;
-    let mut last_position: Option<Vector3> = None;
     let mut mouse_down = false;
+
     let mut mouse_position = Vector2::new(0., 0.);
 
     let start_position = Vector3::new(0.0, 1.6, 0.0);
@@ -154,7 +292,6 @@ async fn run(app: Application, mut events: Events) {
         velocity: Vector3::ZERO,
         radius: 0.06,
         color: Color::new(1.0, 1.0, 1.0, 1.0),
-        intersection_position: Vector3::ZERO,
     };
 
     let mut circle = Mesh::new(&gl);
@@ -175,6 +312,16 @@ async fn run(app: Application, mut events: Events) {
 
     let white = Color::new(1.0, 1.0, 1.0, 1.0);
 
+    let mut level = Level {
+        line_color: Color::new(138.0 / 255.0, 132. / 255.0, 170. / 255.0, 1.0),
+        collectibles: Vec::new(),
+    };
+
+    let mut lines = Lines::new(&gl);
+
+    let mut mouse_playback = MousePlayback::new();
+    let mut recording = true;
+
     loop {
         let event = events.next_event().await;
         match event {
@@ -182,46 +329,35 @@ async fn run(app: Application, mut events: Events) {
             Event::MouseMoved { x, y, .. } => {
                 // When the mouse check if the mouse drawing should be updated.
                 mouse_position = Vector2::new(x, y);
+
                 if mouse_down {
-                    let mouse_pos = screen_to_gl(
+                    let mouse_position = screen_to_world(
                         mouse_position.x,
                         mouse_position.y,
                         &camera,
                         screen_width,
                         screen_height,
                     );
-                    if let Some(last_position_inner) = last_position {
-                        if (last_position_inner - mouse_pos).length() > 0.01 {
-                            line_points.push(Vector3::new(
-                                last_position_inner.x,
-                                last_position_inner.y,
-                                0.0,
-                            ));
-                            line_points.push(Vector3::new(mouse_pos.x, mouse_pos.y, 0.0));
-                            lines::update_mesh_with_line(
-                                &gl,
-                                &mut line_mesh,
-                                &line_points,
-                                LINE_RADIUS,
-                                Vector3::FORWARD,
-                            );
-                            last_position = Some(mouse_pos);
-                        }
-                    } else {
-                        // Add a point
-                        last_position = Some(mouse_pos);
+                    if recording {
+                        mouse_playback
+                            .record_mouse(Vector2::new(mouse_position.x, mouse_position.y));
                     }
+                    lines.add_segment(mouse_position);
                 }
-
-                // Debug test of line intersection
-                let mouse_pos = screen_to_gl(
-                    mouse_position.x,
-                    mouse_position.y,
-                    &camera,
-                    screen_width,
-                    screen_height,
-                );
-                //ball.position = Vector3::new(mouse_pos.x, mouse_pos.y, 0.0);
+            }
+            Event::MouseButtonDown {
+                button: MouseButton::Right,
+                x,
+                y,
+                ..
+            } => {
+                let mouse_pos = screen_to_world(x, y, &camera, screen_width, screen_height);
+                level.collectibles.push(Collectible {
+                    position: mouse_pos,
+                    color: white,
+                    radius: 0.015,
+                    collected: false,
+                });
             }
             Event::MouseButtonDown {
                 button: MouseButton::Left,
@@ -238,21 +374,24 @@ async fn run(app: Application, mut events: Events) {
                 ..
             } => {
                 mouse_down = false;
-                last_position = None;
+                lines.end_segment();
+                if recording {
+                    mouse_playback.record_mouse_up();
+                }
+            }
+            Event::KeyDown { key: Key::P, .. } => {
+                lines.clear();
+                mouse_playback.reset_playback();
+                mouse_playback.playing = true;
             }
             Event::KeyDown { key: Key::E, .. } => {
-                line_points.clear();
-                lines::update_mesh_with_line(
-                    &gl,
-                    &mut line_mesh,
-                    &line_points,
-                    LINE_RADIUS,
-                    Vector3::FORWARD,
-                );
+                lines.clear();
+                lines.update_mesh(&gl);
             }
             Event::KeyDown { key: Key::R, .. } => {
                 ball.position = start_position;
                 ball.velocity = Vector3::ZERO;
+                level.reset();
             }
             Event::WindowResized { width, height, .. } => unsafe {
                 screen_width = width;
@@ -269,8 +408,13 @@ async fn run(app: Application, mut events: Events) {
                 ));
             },
             Event::Draw { .. } => {
+                mouse_playback.increment_frame();
+                if mouse_playback.playing {
+                    mouse_playback.playback(1, &mut lines);
+                }
                 // First update physics
-                ball.ball_physics(&line_points);
+                ball.ball_physics(&lines.line_points);
+                ball.check_for_collectibles(&mut level.collectibles);
 
                 // Clear the screen.
                 unsafe {
@@ -286,9 +430,11 @@ async fn run(app: Application, mut events: Events) {
 
                 // First render the line
                 shader_program.set_matrix(&gl, "u_model", &Matrix4x4::IDENTITY);
-                shader_program.set_color(&gl, "u_color", &white);
+                shader_program.set_color(&gl, "u_color", &level.line_color);
 
-                line_mesh.draw(&gl);
+                // Only updates if necessary
+                lines.update_mesh(&gl);
+                lines.mesh.draw(&gl);
 
                 // Then render the circle
                 shader_program.set_matrix(
@@ -304,19 +450,23 @@ async fn run(app: Application, mut events: Events) {
 
                 circle.draw(&gl);
 
-                // Draw debug circle
-                shader_program.set_matrix(
-                    &gl,
-                    "u_model",
-                    &mat4_from_trs(
-                        ball.intersection_position,
-                        Quaternion::IDENTITY,
-                        Vector3::new_uniform(ball.radius * 0.5),
-                    ),
-                );
-                shader_program.set_color(&gl, "u_color", &ball.color);
+                // Draw collectibles
+                for collectible in &level.collectibles {
+                    if !collectible.collected {
+                        shader_program.set_matrix(
+                            &gl,
+                            "u_model",
+                            &mat4_from_trs(
+                                collectible.position,
+                                Quaternion::IDENTITY,
+                                Vector3::new_uniform(collectible.radius),
+                            ),
+                        );
+                        shader_program.set_color(&gl, "u_color", &ball.color);
 
-                circle.draw(&gl);
+                        circle.draw(&gl);
+                    }
+                }
 
                 // Finally display what we've drawn.
                 // Since we're using web this happens automatically, but on desktop this call is required.
@@ -328,7 +478,13 @@ async fn run(app: Application, mut events: Events) {
     }
 }
 
-fn screen_to_gl(x: f32, y: f32, camera: &Camera, window_width: u32, window_height: u32) -> Vector3 {
+fn screen_to_world(
+    x: f32,
+    y: f32,
+    camera: &Camera,
+    window_width: u32,
+    window_height: u32,
+) -> Vector3 {
     let v = Vector3::new(
         x / (window_width as f32),
         1.0 - (y / (window_height as f32)),
@@ -336,8 +492,10 @@ fn screen_to_gl(x: f32, y: f32, camera: &Camera, window_width: u32, window_heigh
     ) * 2.0
         - Vector3::new(1.0, 1.0, 0.0);
 
-    mat4_transform_point(
+    let mut p = mat4_transform_point(
         &camera.inverse_view,
         mat4_transform_point(&camera.inverse_projection, v),
-    )
+    );
+    p.z = 0.0;
+    p
 }
